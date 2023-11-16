d10c6d6516bdc0b32616bce059119808b8c31d32
Camila <camiart@gmail.com>
2023-11-16 09:27:56 -0300

    exercicio 04

---
+++ src/main/java/com/example/registroMultas/repository/MultaRepository.java
@@ -0,0 +1,9 @@
+package com.example.registroMultas.repository;
+
+import com.example.registroMultas.model.Multa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface MultaRepository extends JpaRepository<Multa, Long> {
+
+}
---
+++ src/main/java/com/example/registroMultas/repository/VeiculoRepository.java
@@ -0,0 +1,9 @@
+package com.example.registroMultas.repository;
+
+import com.example.registroMultas.model.Veiculo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface VeiculoRepository extends JpaRepository<Veiculo, String> {
+
+}
