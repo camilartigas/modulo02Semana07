c9ef0b88b33f217f00c3f96d22fb17359fa7bb33
Camila <camiart@gmail.com>
2023-11-16 08:12:36 -0300

    exercicio 01

---
+++ src/main/java/com/example/registroMultas/RegistroMultasApplication.java
@@ -0,0 +1,13 @@
+package com.example.registroMultas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroMultasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroMultasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/registroMultas/RegistroMultasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.registroMultas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroMultasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
